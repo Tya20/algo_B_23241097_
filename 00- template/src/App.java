1e1f8c040c1337669d52790d0a2d69170c9e0a00
Tya20 <146092759+Tya20@users.noreply.github.com>
2023-10-31 13:48:12 +0800

    kuliah algo 31/10/23

---
+++ 00- template/src/App.java
@@ -0,0 +1,35 @@
+public class App  {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+         //program Biodata mahasiswa 
+         //deklarasi variabel
+         String nama;
+         String nim;
+         String tgl_lahir;
+         String alamat;
+         float berat_badan;
+         float tinggi_badan;
+         
+         //mengisi variabel
+         nama = "mustiati";
+         nim = "23241097";
+         tgl_lahir = "20 juli 2005";
+         alamat = "bima";
+         berat_badan = 45f;
+         tinggi_badan = 160;
+
+         // cetak biodata 
+         System.out.println("==========================");
+          System.out.println("nama" + nama);
+          System.out.println("nim :" + nim);
+          System.out.println("tgl_lahir:" + tgl_lahir);
+          System.out.println("alamat :" + alamat);
+          System.out.println("berat badan :" + berat_badan);
+          System.out.println("tinngi badan :" + tinggi_badan);
+          System.out.println("============================");
+          
+
+         
+
+    }
+}
